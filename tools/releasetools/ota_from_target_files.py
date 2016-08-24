9276dc0631fcb42b1886a91459a7bd0aedcbbaef
beanstown106 <nbenis106@gmail.com>
2016-08-24 00:50:31 -0400

    clean up rom flash & add purenexus flare *drop the target:build fingerprint text *drop the patching system image unconditionally text *drop the verifying system image text *changed the verified system image text *add flashing system text *add flashing kernel text *add some purenexus flare
    Change-Id: Ieef19d155a2f77b1d93b45e94957f3ed2653ac9e

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -625,8 +625,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
   # Dump fingerprints
-  script.Print("Target: %s" % CalculateFingerprint(
-      oem_props, oem_dict, OPTIONS.info_dict))
+  #script.Print("Target: %s" % CalculateFingerprint(
+  #    oem_props, oem_dict, OPTIONS.info_dict))
+  script.Print("********************")
+  script.Print("**   Pure NeXus   **")
+  script.Print("********************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
@@ -722,6 +725,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     if block_based:
       script.Unmount("/system")
 
+  script.Print(" ")
+  script.Print("Flashing Kernel..")
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
