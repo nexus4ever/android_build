23c259fc4aec586c52521308d3f7ff35b57df813
xyyx <xyyx@mail.ru>
2016-08-31 08:15:40 +0800

    Nitrogen Logo

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -657,6 +657,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    build = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+
+    script.Print("*  * *** *** **** **** **** *** *  *  **** ***");
+    script.Print("** *  *   *  *  * *  * *    *   ** *  *  * *");
+    script.Print("* **  *   *  **** *  * * ** *** * **  *  * ***");
+    script.Print("*  *  *   *  * *  *  * *  * *   *  *  *  *   *");
+    script.Print("*  * ***  *  * *  **** **** *** *  *  **** ***");
+    script.Print("- By xyyx & Mr.MEX --------------- Android 7 -");
+    script.Print(" Compiled: %s "%(build));
+    script.Print(" For: %s   "%(model));
+    script.Print("----------------------------------------------");
+
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   system_items = ItemSet("system", "META/filesystem_config.txt")
